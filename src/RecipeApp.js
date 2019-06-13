d8a7d6ab8fe3793be81bba59950d8a6dbc2c95c4
Zachary <zachuht@gmail.com>
2019-06-13 09:21:16 -0700

    first commit

---
+++ src/RecipeApp.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import Recipe from './Recipe';
+import Navbar from './Navbar';
+import RecipeList from './RecipeList';
+import './RecipeApp.css';
+
+class RecipeApp extends Component {
+  render() {
+    return (
+        <div className="App">
+          <Navbar />
+          <RecipeList />
+        </div>
+    );
+  }
+}
+
+export default RecipeApp;
